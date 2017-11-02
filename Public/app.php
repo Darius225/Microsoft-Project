2c0f43ed3ff66362c5522bfded5eba945f4cb20b
Darius225 <darius.grigore.pop10@gmail.com>
2017-11-02 21:36:56 +0000

    Update app.php

--- Public/app.php
+++ Public/app.php
@@ -4,6 +4,8 @@ $content = '' ;
 $tags = '' ;
 $category = '' ;
 $errors = [] ;
+$log_out = $_POST ['log_out'] ;
+echo $log_out ;
  if ( is_post_request() )
  {
     $title = $_POST['title'] ?? '' ;
@@ -32,7 +34,7 @@ $errors = [] ;
      <div class="nav-wrapper amber darken-1" style="color:black;">
        <div id="nav-mobile" class="right">
          <span class="brand-logo" style="margin-left:15px; color:black; text-align: right;"><b>Keep</b></span>
-         <input class="waves-light btn amber" type="submit" name="log_out" value="log out" style="color: black; margin: 15px;">
+         <input class="waves-light btn amber" type="submit" name="log_out" style="color: black; margin: 15px;">
        </div>
      </div>
    </nav>
