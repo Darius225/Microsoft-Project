f3e061a775a4ee60e79de9d027c54b2f238a83d0
VoytechG <voytech97@gmail.com>
2017-11-02 13:43:35 +0000

    styling login

--- Public/login.php
+++ Public/login.php
@@ -45,16 +45,51 @@ if(is_post_request())
      }
 }
 ?>
-<div id="content">
-  <h1>Log in</h1>
 
-  <form action="" method="post">
-    Username:<br />
-    <input type="text" name="username" value="<?php $username; ?>" /><br />
-    Password:<br />
-    <input type="password" name="password" value="" /><br />
-    <input type="submit" value="Login"  />
-    <input type="submit" name="register" value="Register" />
-  </form>
+<!DOCTYPE html>
+<!-- saved from url=(0021)https://gvoy.tech/ms/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-</div>
+  <title>Voytech</title>
+  <meta name="description" conent="My personal website summarising my projects, passions and achievements.">
+  <meta name="keywords" content="computer science, games development, design, music">
+  <meta name="author" content="Wojciech Golaszewski">
+  <meta http-equiv="X-Ua-Compatible" content="IE=edge, chrome=1">
+
+  <link href="./Voytech_files/css" rel="stylesheet">
+  <link rel="stylesheet" href="./Voytech_files/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <link rel="stylesheet" href="./Voytech_files/msstyling.css">
+  <style>
+
+   .theblue {
+     color: #1daef7;
+   }
+
+   .centered {
+     position: relative;
+     transform: translate(0, -50%);
+     text-align: center;
+    }
+
+    .middle {
+      top: 50%;
+    }
+
+  </style>
+
+</head>
+<body>
+  <div id="content">
+    <h1>Log in</h1>
+
+    <form action="" method="post">
+      Username:<br />
+      <input type="text" name="username" value="<?php $username; ?>" /><br />
+      Password:<br />
+      <input type="password" name="password" value="" /><br />
+      <input type="submit" value="Login"  />
+      <input type="submit" name="register" value="Register" />
+    </form>
+
+  </div>
+</body>
